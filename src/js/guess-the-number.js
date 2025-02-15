90d011654f38b606c48b4354dcef34e71f097ef7
svedova1899 <asvedova49@gmail.com>
2025-02-15 20:16:20 +0200

    sass, js

---
+++ src/js/guess-the-number.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const input = document.getElementById("number");
+    const button = document.getElementById("guess-button");
+    const result = document.getElementById("resultat");
+
+    const computerChoice = Math.floor(Math.random() * 10) + 1;
+
+    button.addEventListener("click", () => {
+        event.preventDefault();
+        const userChoice = parseInt(input.value, 10);
+        if (!isNaN(userChoice) && userChoice >= 1 && userChoice <= 10) {
+            if (userChoice === computerChoice) {
+                result.innerHTML= `Вітаю, ви вгадали число! ${computerChoice}`;
+                result.className= "green-paragraph";
+            } else{
+                result.innerHTML= `Ви програли, комп'ютер загадав ${computerChoice}.`;
+                result.className = "red-paragraph";
+            }
+        } else {
+            result.innerHTML = "Введіть число від 1 до 10!";
+            result.className = "grey-paragraph";
+        }
+    });
+});
