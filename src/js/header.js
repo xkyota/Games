95c9feb25b6a0e235f35fc9fe401a983d4bec5bb
xkyota <max1mkasss.mail@gmail.com>
2025-02-16 17:22:46 +0200

    Added header.js (50% done)

---
+++ src/js/header.js
@@ -0,0 +1,14 @@
+// const switchTheme = document.getElementById('switch');
+// const containerBackground = document.querySelector('.container-background');
+
+// switchTheme.addEventListener('click', function () {
+//   if (containerBackground.style.backgroundColor === 'black') {
+//     containerBackground.style.backgroundColor = 'white';
+//     containerBackground.style.color = 'black';
+//     document.body.style.backgroundColor = 'white';
+//   } else {
+//     containerBackground.style.backgroundColor = 'black';
+//     containerBackground.style.color = 'white';
+//     document.body.style.backgroundColor = 'black';
+//   }
+// });
--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-import "./js/calculator.js"; 
-import "./js/choose-a-scientists.js";
-import "./js/football.js";
+import './js/calculator.js';
+import './js/choose-a-scientists.js';
+import './js/football.js';
 // import "./js/rock-paper-scissors.js";
-import "./js/footer.js";
-import "./js/google-dinosaur.js";
-import "./js/guess-the-number.js";
-import "./js/header.js";
-import "./js/is-year-leap.js";
-import "./js/modal-hello.js";
-import "./js/modal-subscribe.js";
-import "./js/our-team.js";
-import "./js/the-largest-of-three-numbers.js";
-import "./js/time-converter.js";
-
+import './js/footer.js';
+import './js/google-dinosaur.js';
+import './js/guess-the-number.js';
+import './js/header.js';
+import './js/is-year-leap.js';
+import './js/modal-hello.js';
+import './js/modal-subscribe.js';
+import './js/our-team.js';
+import './js/the-largest-of-three-numbers.js';
+import './js/time-converter.js';
