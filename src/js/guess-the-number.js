df484c4a642326778263f7f418fa85b7b546df0c
danikkkkk12 <danilmalyk20@gmail.com>
2025-02-18 23:41:40 +0200

    fix bag random number

--- src/js/guess-the-number.js
+++ src/js/guess-the-number.js
@@ -3,22 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const button = document.getElementById("guess-button");
     const result = document.getElementById("resultat");
 
-    const computerChoice = Math.floor(Math.random() * 10) + 1;
-
-    button.addEventListener("click", () => {
+    button.addEventListener("click", (event) => {
         event.preventDefault();
+
+        const computerChoice = Math.floor(Math.random() * 10) + 1; 
         const userChoice = parseInt(input.value, 10);
+
         if (!isNaN(userChoice) && userChoice >= 1 && userChoice <= 10) {
             if (userChoice === computerChoice) {
-                result.innerHTML= `Вітаю, ви вгадали число! ${computerChoice}`;
-                result.className= "green-paragraph";
-            } else{
-                result.innerHTML= `Ви програли, комп'ютер загадав ${computerChoice}.`;
+                result.innerHTML = `Вітаю, ви вгадали число! ${computerChoice}`;
+                result.className = "green-paragraph";
+            } else {
+                result.innerHTML = `Ви програли, комп'ютер загадав ${computerChoice}.`;
                 result.className = "red-paragraph";
             }
         } else {
             result.innerHTML = "Введіть число від 1 до 10!";
             result.className = "grey-paragraph";
         }
+
+        input.value = "";
     });
 });
