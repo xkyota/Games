d71b21747379ebcdf5f0e091398f1edfc023efdf
stasiv23 <dan.stasiv@gmail.com>
2025-02-27 20:57:28 +0200

    fix

--- src/js/the-largest-of-three-numbers.js
+++ src/js/the-largest-of-three-numbers.js
@@ -3,7 +3,7 @@ document.getElementById("findMaxBtn").addEventListener("click", function() {
     let num2 = parseFloat(document.getElementById("num2").value);
     let num3 = parseFloat(document.getElementById("num3").value);
 
-    if (isNaN(num1)  isNaN(num2)  isNaN(num3)) {
+    if let maxNumber = Math.max(num1, num2, num3);
         document.getElementById("result").innerText = "Будь ласка, введіть всі числа!";
         return;
     }
