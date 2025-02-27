a088b6be642c1aabef3807ce41b3529c6adf883c
xkyota <max1mkasss.mail@gmail.com>
2025-02-27 21:03:03 +0200

    fix in section largest number

--- src/js/the-largest-of-three-numbers.js
+++ src/js/the-largest-of-three-numbers.js
@@ -3,11 +3,7 @@ document.getElementById("findMaxBtn").addEventListener("click", function() {
     let num2 = parseFloat(document.getElementById("num2").value);
     let num3 = parseFloat(document.getElementById("num3").value);
 
-    if let maxNumber = Math.max(num1, num2, num3);
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3)) {
         document.getElementById("result").innerText = "Будь ласка, введіть всі числа!";
-        return;
     }
-
-    let maxNumber = Math.max(num1, num2, num3);
-    document.getElementById("result").innerText = Найбільше число: ${maxNumber};
-});
+}); 
