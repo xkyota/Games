acdd367e927d416c8bb9172c9eb140075ff699ca
xkyota <max1mkasss.mail@gmail.com>
2025-03-13 08:57:42 +0200

    fixed section modal-hello

--- src/js/modal-hello.js
+++ src/js/modal-hello.js
@@ -1,20 +1,23 @@
 const closeButton = document.querySelector('.backdrop-page__close');
-const submitButton = document.querySelector(".backdrop-page__save")
+const submitButton = document.querySelector(".backdrop-page__save");
 const modal = document.querySelector('.backdrop-page');
-function closeModal() {
-    modal.classList.add('first-page-is-hidden');
-}
-closeButton.addEventListener('click', function() {
-    modal.style.display = 'none';
-});
-submitButton.addEventListener('click', function() {
-    modal.style.display = 'none';
-});
 
+const userInput = document.querySelector(".backdrop-page__input");
+const userName = document.getElementById("user-name"); 
+const submitName = document.querySelector(".backdrop-page__save");
 
-const name = document.querySelector(".backdrop-page__input")
-const userName = document.querySelector(".user-name")
-const submitName = document.querySelector(".backdrop-page__save")
 submitName.addEventListener("click", (event) => {
-    userName.textContent = name.value
-})
+    event.preventDefault(); 
+    userName.textContent = ", "  + userInput.value.trim(); 
+    modal.style.display = 'none'; 
+}); 
+
+closeButton.addEventListener('click', function (event) {
+    event.preventDefault(); 
+    modal.style.display = 'none';
+});
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault(); 
+    modal.style.display = 'none';
+});
