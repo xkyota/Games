ef755dc9cc3c1a9ee53a8eec05c50ae9a93e3577
xkyota <max1mkasss.mail@gmail.com>
2025-03-13 16:15:04 +0200

    fixed some errors

---
+++ src/js/is-year-leap.js
@@ -0,0 +1,23 @@
+const checkYearInput = document.querySelector('.input__leap')
+let yesNoLeap = document.getElementById('yes-no-leap');
+document.querySelector('.input-but__leap').addEventListener('click', function() {
+    let userInput = document.getElementById('leap-input').value;
+
+    if (userInput) {
+        if (userInput % 4 === 0) {
+            checkYearInput.value = ""
+            yesNoLeap.textContent = "Ви народились у високосний рік";
+            yesNoLeap.style.color = "#039900";
+        } else {
+            checkYearInput.value = ""
+            yesNoLeap.textContent = "Ви не народились у високосний рік";
+            yesNoLeap.style.color = "#990000";
+        }
+        yesNoLeap.style.transition = "all 500ms ease";
+    } else {
+        checkYearInput.value = ""
+        yesNoLeap.textContent = "Будь ласка, введіть рік народження";
+        yesNoLeap.style.color = "#000000";
+    }
+    yesNoLeap.style.opacity = "1";
+});
