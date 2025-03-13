2258a832c328090e5f4bf768887c4450a833d6ea
xkyota <max1mkasss.mail@gmail.com>
2025-03-13 16:41:02 +0200

    fixed section is year leap

--- src/js/is-year-leap.js
+++ src/js/is-year-leap.js
@@ -4,7 +4,12 @@ document.querySelector('.input-but__leap').addEventListener('click', function() 
     let userInput = document.getElementById('leap-input').value;
 
     if (userInput) {
-        if (userInput % 4 === 0) {
+        if (+userInput > 2025) { 
+            checkYearInput.value = ""
+            yesNoLeap.textContent = "Не обманюйте будь-ласка :(";
+            yesNoLeap.style.color = "#990000";
+        }
+        else if(userInput % 4 === 0) {
             checkYearInput.value = ""
             yesNoLeap.textContent = "Ви народились у високосний рік";
             yesNoLeap.style.color = "#039900";
